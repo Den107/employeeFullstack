8cb0f2bd61c917ec99efc8955debaf7025b43024
Denis Kravchuk <deniskrv@gmail.com>
2023-05-19 18:47:49 +0300

    настроил роуты на фронте, сделал базу страниц логина и регистрации

---
+++ client/src/paths.ts
@@ -0,0 +1,11 @@
+export const Paths = {
+    home: '/',
+    employeeAdd: '/employees',
+    employeeEdit: '/employees',
+    employeeRemove: '/employees',
+    employeeGetOne: '/employees',
+    employeeGetAll: '/employees',
+    status: '/status',
+    login: '/login',
+    register: '/register'
+} as const;
