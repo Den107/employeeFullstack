40a676c75024616ca6ae5159aecc6e41b76092e0
Denis Kravchuk <deniskrv@gmail.com>
2023-05-23 11:56:00 +0300

    сделал страницу регистрации

--- client/src/pages/register/index.tsx
+++ client/src/pages/register/index.tsx
@@ -1,10 +1,32 @@
 import React from 'react';
+import {Card, Form, Row, Space, Typography} from "antd";
+import CustomInput from "../../components/custom-input";
+import CustomPasswordInput from "../../components/custom-password-input";
+import CustomButton from "../../components/custom-button";
+import {Link} from "react-router-dom";
+import {Paths} from "../../paths";
+import Layout from "../../components/layout";
 
 const Register = () => {
     return (
-        <div>
-            Register
-        </div>
+        <Layout>
+            <Row align={'middle'} justify={'center'}>
+                <Card title={'Регистрация'} style={{width: '30rem'}}>
+                    <Form onFinish={() => null}>
+                        <CustomInput name={'name'} placeholder={'Имя'}/>
+                        <CustomInput name={'email'} placeholder={'Email'} type={'email'}/>
+                        <CustomPasswordInput name={'password'} placeholder={'Пароль'}/>
+                        <CustomPasswordInput name={'confirmPassword'} placeholder={'Повторите пароль'}/>
+                        <CustomButton type={'primary'} htmlType={'submit'}>Зарегистрироваться</CustomButton>
+                    </Form>
+                    <Space size={'large'} direction={'vertical'}>
+                        <Typography.Text>
+                            Уже зарегистрированы? <Link to={Paths.login}>Войти</Link>
+                        </Typography.Text>
+                    </Space>
+                </Card>
+            </Row>
+        </Layout>
     );
 };
 
